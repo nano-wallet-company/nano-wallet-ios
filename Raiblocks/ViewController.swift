18caa3bdba3cc819a71d6aed2de2328b8b347b7e
Zack Shapiro <zack@zackshapiro.com>
2017-11-22 14:21:48 -0500

    Initial Commit

---
+++ Raiblocks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Raiblocks
+//
+//  Created by Zack Shapiro on 11/22/17.
+//  Copyright © 2017 Zack Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
