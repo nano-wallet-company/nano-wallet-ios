5786404e496527734609b6b20fcbf92c25829c11
Zack Shapiro <zack@zackshapiro.com>
2018-02-26 14:20:52 -0500

    add support for xrb: in url query extension

--- Raiblocks/Extensions/URL+Extensions.swift
+++ Raiblocks/Extensions/URL+Extensions.swift
@@ -11,7 +11,22 @@ extension URL {
     var queryDictionary: [String: String]? {
         guard let query = URLComponents(string: self.absoluteString)?.query else { return nil}
 
+        // TODO: add nano_ support later
         var queryStrings: [String: String] = [:]
+        for item in query.components(separatedBy: ":") {
+            if item.contains("xrb_") {
+                let items = item.split(separator: "?")
+                queryStrings["address"] = String(items[0])
+
+                for i in items {
+                    if i.contains("amount=") {
+                        let val = i.split(separator: "=")[1]
+                        queryStrings["amount"] = String(val)
+                    }
+                }
+            }
+        }
+
         for pair in query.components(separatedBy: "&") {
             let key = pair.components(separatedBy: "=")[0]
 
